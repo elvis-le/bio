f02d41a32fd8ffb93c816641b3eed4d06e4f3d39
elvis-le <khanh321zx@gmail.com>
2025-08-09 17:08:26 +0700

    add .env frontend

--- my-frontend/src/pages/ProductPage.js
+++ my-frontend/src/pages/ProductPage.js
@@ -31,7 +31,7 @@ const ProductPage = () => {
         const fetchCatalogs = async () => {
             try {
 
-                const response = await axios.get('http://localhost:5000/api/catalogs', {
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/catalogs`, {
                     headers: {},
                 });
                 setCatalogs(response.data);
@@ -47,7 +47,7 @@ const ProductPage = () => {
         const fetchProducts = async () => {
             try {
 
-                const response = await axios.get('http://localhost:5000/api/products', {
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/products`, {
                     headers: {},
                 });
                 setProducts(response.data);
@@ -94,7 +94,7 @@ const ProductPage = () => {
 
     const handleCatalogSelect = async (catalogId) => {
         try {
-            const response = await axios.get('http://localhost:5000/api/subcategories', {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/subcategories`, {
                 headers: {},
                 params: {catalog_id: catalogId},
             });
